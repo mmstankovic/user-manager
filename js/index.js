c231f6a2233d80b2dd0d3c0264a0b2bbb27595c7
Milos Stankovic <stanko902@gmail.com>
2026-04-25 17:26:18 +0200

    Added edit user functionality

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+const container = document.querySelector('#container')
 const btnLoad = document.querySelector('#loadBtn')
 const statusMessage = document.querySelector('#status')
 const userList = document.querySelector('#userList')
@@ -10,6 +11,8 @@ let users = []
 let selectedUser = null
 let searchTerm = ''
 let selectedSort = ''
+let isEditing = false
+let selectedUserToEdit = null
 
 async function fetchUsers() {
     loading = true
@@ -59,10 +62,10 @@ function render() {
     }
 
     if (selectedSort) {
-      filtered.sort((a,b) => {
-          return selectedSort === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
-      })  
-    } 
+        filtered.sort((a, b) => {
+            return selectedSort === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+        })
+    }
 
     sortContainer.querySelectorAll('button').forEach((btn) => {
         btn.classList.toggle('active', btn.id === selectedSort)
@@ -87,10 +90,59 @@ function render() {
             const website = document.createElement('p')
             website.textContent = user.website
 
+
             div.appendChild(phone)
             div.appendChild(website)
 
+            if(!selectedUserToEdit || selectedUserToEdit.id !== user.id) {
+                const btnEdit = document.createElement('button')
+                btnEdit.textContent = 'Edit'
+                btnEdit.classList.add('edit-btn')
+
+                div.appendChild(btnEdit)
+            }
+
             li.appendChild(div)
+
+            if(isEditing && selectedUserToEdit?.id === user.id) {
+                const form = document.createElement('form')
+                form.id = 'edit-form'
+
+                form.addEventListener('submit', function(e) {
+                    e.preventDefault()
+
+                    const input = e.target.querySelector('input')
+
+                    updateUserEmail(input.value)
+
+                    input.value = ''
+                }) 
+
+                const input = document.createElement('input')
+                input.type = 'text'
+                input.placeholder = 'Update email'
+                input.id = 'update-email'
+                input.value = user.email
+
+                const actionsDiv = document.createElement('div')
+
+                const btnCancel = document.createElement('button')
+                btnCancel.textContent = 'Cancel'
+                btnCancel.classList.add('cancel-btn')
+
+                const btnSave = document.createElement('button')
+                btnSave.textContent = 'Save'
+                btnSave.type = 'submit'
+                btnSave.classList.add('save-btn')
+
+                actionsDiv.appendChild(btnCancel)
+                actionsDiv.appendChild(btnSave)
+
+                form.appendChild(input)
+                form.appendChild(actionsDiv)
+
+                li.appendChild(form)
+            }
         }
 
         userList.appendChild(li)
@@ -99,6 +151,24 @@ function render() {
 
 render()
 
+function updateUserEmail(val) {
+    if(val.trim()=== '') return 
+
+    users = users.map((user) => user.id === selectedUserToEdit.id ? {...user, email: val} : user)
+
+    isEditing = false 
+    selectedUserToEdit = null 
+
+    render()
+}
+
+function cancelUpdateUserEmail() {
+    isEditing = false 
+    selectedUserToEdit = null
+
+    render()
+}
+
 btnLoad.addEventListener('click', function () {
     fetchUsers()
 })
@@ -106,19 +176,34 @@ btnLoad.addEventListener('click', function () {
 userList.addEventListener('click', function (e) {
     const li = e.target.closest('li')
 
-    if (li) {
+    if(!li) return
+
+    if(e.target.closest('.cancel-btn')) {
+        cancelUpdateUserEmail()
+        return
+    }
+
+    if (e.target.closest('.edit-btn')) {
         const id = li.dataset.id
         const user = users.find((item) => item.id === Number(id))
+        isEditing = true
+        selectedUserToEdit = user
 
-        if (!user) return
-
-        if (selectedUser?.id === user.id) {
-            selectedUser = null
-        } else {
-            selectedUser = user
-        }
         render()
+        return 
     }
+    if(e.target.closest('#edit-form')) {
+        return
+    }
+
+    const id = li.dataset.id
+    const user = users.find((item) => item.id === Number(id))
+
+    if (!user) return
+
+    selectedUser = selectedUser?.id === user.id ? null : user
+    
+    render()
 })
 
 searchInput.addEventListener('input', function (e) {
@@ -129,10 +214,10 @@ searchInput.addEventListener('input', function (e) {
 
 sortContainer.addEventListener('click', function (e) {
     const btn = e.target.closest('button')
-    if(!btn) return 
+    if (!btn) return
 
     const id = btn.id
-    
+
     if (id === 'asc') {
         selectedSort = 'asc'
     }
